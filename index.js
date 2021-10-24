f52668e83573931a0675801904b3d09ff298687b
Jagriti Sikka <jsikka@Jagritis-Air.attlocal.net>
2021-10-23 22:52:45 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  console.log("Here");
+  document.getElementById("overlay").style.display = "block";
+}
+// document.getElementsByClassName("main-button").addEventListener("click", function() {
+//   // document.getElementById("demo").innerHTML = "Hello World";
+//   console.log("Here");
+//   alert(1);
+// });
